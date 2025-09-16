bb3b4b0e4837e0120dde66f640111206665a18e6
MIhaliskar <pepperblackmk@gmail.com>
2025-09-16 22:15:54 +0300

    Added new commands

--- main.py
+++ main.py
@@ -36,8 +36,17 @@ def talk_openai(question, instruction = "Give a short reply"):
 
     return response.output_text
 
+def code_openai(question, instruction):
+    response = client.responses.create(
+        model="gpt-5",
+        instructions=f"You are a bot named Jarvis on a discord server of some friends studying computer science. You are an expert on the field and you can provide a perfect answer to any question regarding our studies. Please {instruction}",
+        input = question
+    )
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    return response.output_text
+
+
+handler = logging.FileHandler(filename='Github_Bot/discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
@@ -78,6 +87,43 @@ async def ask(ctx, *, msg):
 
     await ctx.send(f"{ctx.author.mention} {reply}")
 
+
+@bot.command()
+async def jarvis(ctx):
+    await ctx.send("/hello -> Say hello to the bot \n /ask message -> Ask a question to chat gpt \n /jarvis -> Get a list of all the commands " \
+    "\n /code -> Ask chat gpt for coding help \n /vibe -> Fix code using chat gpt by providing a file containing the code \n /solve -> Solve assignments using chat gpt " \
+    "\n /summarize -> Get a summary of lecture slides \n /quiz -> Get a quiz on slides")
+
+@bot.command()
+async def code(ctx, *, msg):
+    user_name = ctx.author.display_name
+    message = msg
+    instruct = f"give a complete and friendly reply to the user named {user_name}, But keep it correct, informative and keep it under 1000 characters"
+    reply = ""
+    async with ctx.typing():
+        try:
+            reply = code_openai(message, instruct)
+        except Exception as e:
+            await ctx.send(f"Sorry {ctx.author.mention}, I run into an error: {e}")
+            return
+
+    await ctx.send(f"{ctx.author.mention} {reply}")
+
+@bot.command()
+async def vibe(ctx, *, msg):
+    await ctx.send(f"{ctx.author.mention}, this command is not yet available")
+
+@bot.command()
+async def solve(ctx, *, msg):
+    await ctx.send(f"{ctx.author.mention}, this command is not yet available")
+
+@bot.command()
+async def summarize(ctx, *, msg):
+    await ctx.send(f"{ctx.author.mention}, this command is not yet available")
+
+@bot.command()
+async def quiz(ctx, *, msg):
+    await ctx.send(f"{ctx.author.mention}, this command is not yet available")
 
 
 bot.run(discord_token, log_handler=handler, log_level=logging.DEBUG)
